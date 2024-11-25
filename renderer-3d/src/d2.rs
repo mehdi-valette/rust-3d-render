122c5a690747618e74418caa8308aa29712d22b0
Mehdi Valette <mehdi.valette@gmail.com>
2024-11-25 17:59:51 +0100

    feat: draw 2D lines

--- renderer-3d/src/d2.rs
+++ renderer-3d/src/d2.rs
@@ -1,8 +1,66 @@
 type WindowBuffer<'a> = [u32];
 
+pub struct Vec2D {
+    x: i32,
+    y: i32
+}
+
+impl Vec2D {
+    pub fn new(x: i32, y: i32) -> Vec2D {
+        Vec2D{x, y}
+    }
+    
+    pub fn add(&mut self, point: &Vec2D) {
+        self.x = self.x + point.x;
+        self.y = self.y + point.y;
+    }
+
+    pub fn substract(&mut self, point: &Vec2D) {
+        self.x = self.x - point.x;
+        self.y = self.y - point.y;
+    }
+
+    pub fn to_index(&self, width: u32) -> Result<usize, &'static str> {
+        if self.x < 0 || self.y < 0 {
+            return Err("x and y must be >= 0");
+        }
+
+        Ok((self.y * width as i32 + self.x) as usize)
+    }
+
+    pub fn turn(&mut self, reference: &Vec2D, angle: f32) {
+        self.substract(reference);
+        let adapted_angle = -angle;
+
+        let precise_x = self.x as f32;
+        let precise_y = self.y as f32;
+
+        self.x = (precise_x * adapted_angle.cos() - precise_y * adapted_angle.sin()) as i32;
+        self.y = (precise_x * adapted_angle.sin() + precise_y * adapted_angle.cos()) as i32;
+
+        self.add(reference);
+    }
+}
+
+pub struct Color {
+    red: u8,
+    green: u8,
+    blue: u8
+}
+
+impl Color {
+    pub fn new(red: u8, green: u8, blue: u8) -> Color {
+        Color{red, green, blue}
+    }
+
+    pub fn to_u32(&self) -> u32{
+        (self.red as u32) << 16 | (self.green as u32) << 8 | self.blue as u32
+    }
+}
+
 pub struct Drawing<'a> {
-    pub buffer: &'a mut WindowBuffer<'a>,
-    pub width: u32
+    buffer: &'a mut WindowBuffer<'a>,
+    width: u32
 }
 
 impl<'a> Drawing<'a> {
@@ -16,17 +74,65 @@ impl<'a> Drawing<'a> {
         }
     }
 
-    pub fn middle_white_point(&mut self) {
-        let height = self.buffer.len() as u32 / self.width;
-    
-        self.buffer[self.coord(self.width / 2, height / 2)] = rgb(255, 255, 255);
+    pub fn draw_line(&mut self, point_1: &Vec2D, point_2: &Vec2D, color: &Color) {
+        let (first_point, second_point) = {
+            if point_1.x <= point_2.x {
+                (point_1, point_2)
+            } else {
+                (point_2, point_1)
+            }            
+        };
+
+        let delta_x = second_point.x - first_point.x;
+        let delta_y = second_point.y as i32 - first_point.y as i32;
+
+        if delta_x == 0 {
+            let y_range = {
+                if first_point.y < second_point.y {
+                    first_point.y..=second_point.y
+                } else {
+                    second_point.y..=first_point.y
+                }
+            };
+
+            for y in y_range {
+                let coord = Vec2D::new(first_point.x, y);
+                self.draw_pixel(&coord, &color)
+            }
+
+            return
+        }
+
+        let slope = delta_y as f32 / delta_x as f32;
+
+        for x in 0..delta_x {
+            let current_y = (x as f32 * slope) as i32;
+            let next_y = ((x+1) as f32 * slope) as i32;
+
+            // println!("delta x: {}, delta y: {}, slope: {}, x: {}, current y: {}, next y: {}", delta_x, delta_y, slope, x, current_y, next_y);
+
+            let range_y = {
+                if current_y < next_y {
+                    current_y..=next_y
+                } else {
+                    next_y..=current_y
+                }
+            };
+
+            for y in range_y {
+                let mut coord = Vec2D::new(x, y);
+                coord.add(&first_point);
+                self.draw_pixel(&coord, &color)
+            }
+        }
     }
 
-    fn coord(&self, x: u32, y: u32) -> usize {
-        (x + y * self.width) as usize
+    pub fn draw_pixel(&mut self, coord: &Vec2D, color: &Color) {
+        match coord.to_index(self.width) {
+            Ok(index) => {
+                self.buffer[index] = color.to_u32();
+            },
+            _ => {}
+        }
     }
-}
-
-fn rgb(red: u8, green: u8, blue: u8) -> u32 {
-    (red as u32) << 16 | (green as u32) << 8 | blue as u32
 }
--- renderer-3d/src/main.rs
+++ renderer-3d/src/main.rs
@@ -1,7 +1,7 @@
-use std::{num::NonZeroU32, rc::Rc};
+use std::{f32::consts::PI, num::NonZeroU32, rc::Rc};
 use softbuffer::Surface;
 use winit::{
-    application::ApplicationHandler, dpi::LogicalSize, event_loop::EventLoop, keyboard::{KeyCode, PhysicalKey}, window::Window
+    application::ApplicationHandler, event_loop::EventLoop, keyboard::{KeyCode, PhysicalKey}, window::Window
 };
 
 mod d2;
@@ -24,13 +24,9 @@ struct App {
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        let width = 800;
-        let height = 400;
-
         let window_attributes =
             Window::default_attributes()
-            .with_title("3D renderer")
-            .with_inner_size(LogicalSize{width, height});
+            .with_title("3D renderer");
 
         self.window = Some(Rc::new(event_loop.create_window(window_attributes).unwrap()));
 
@@ -73,7 +69,19 @@ impl ApplicationHandler for App {
                 {
                     let mut drawing = d2::Drawing::new(buffer.as_mut(), width);
                     drawing.all_black();
-                    drawing.middle_white_point();
+
+                    let middle_coord = d2::Vec2D::new(width as i32 /2, height as i32 / 2);
+                    let middle_color = d2::Color::new(100,100,100);
+
+                    drawing.draw_pixel(&middle_coord, &middle_color);
+
+                    let point_1 = d2::Vec2D::new(100, 100);
+                    let mut point_2 = d2::Vec2D::new(200, 100);
+
+                    point_2.turn(&point_1, 1.5*PI);
+                    let color = d2::Color::new(255,255,0);
+
+                    drawing.draw_line(&point_1, &point_2, &color);
                 }
 
                 buffer.present().unwrap();
